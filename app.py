56b6506d997558ccbdb2faf33845397b2daad208
Kusuma Gali <97952344+Kusumagali@users.noreply.github.com>
2025-12-01 11:26:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,8 @@ def answer_request(req_id):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+
+
 
