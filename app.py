4aaf89954c5d1acba9e9442331d9c39afc9d0843
Kusuma Gali <97952344+Kusumagali@users.noreply.github.com>
2025-12-01 09:49:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,9 @@ def update_request(req_id):
 # ------------------- MAIN -------------------
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
+
+
 
 
 
